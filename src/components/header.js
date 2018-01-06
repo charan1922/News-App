d100f5e8b926fffc6ab00741500bf5b371252312
Charan Teja <charaneie22@gmail.com>
2018-01-06 15:28:57 +0530

    Header Section  Completed

--- src/components/header.js
+++ src/components/header.js
@@ -5,21 +5,27 @@ class Header extends Component {
   render() {
     return (
       <View style={styles.header}>
-        <Text>Albums!</Text>
+        <Text style={styles.textStyle}>News</Text>
       </View>
     );
   }
 }
 
 const styles = StyleSheet.create({
-    header:{
-justifyContent: 'center',
-alignItems: 'center',
-height: 60,
-backgroundColor: '#C64848',
-paddingTop: 15
-}
-})
+  header: {
+    justifyContent: "center",
+    alignItems: "center",
+    height: 60,
+    backgroundColor: "#C64848",
+   borderTopRightRadius:4,
+   borderTopLeftRadius:4,
+  },
+  textStyle:{
+    fontSize: 16,
+    fontWeight: '600',
+    color: '#FFFFFF',
+
+  }
+});
 
 export default Header;
-
