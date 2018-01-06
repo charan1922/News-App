bca2ec07367d7e6f1b125afff41b24006e767c1d
Charan Teja <charaneie22@gmail.com>
2018-01-06 20:50:29 +0530

    added news detail component

---
+++ src/components/NewsDetail.js
@@ -0,0 +1,50 @@
+import React from 'react'
+import { View,Text,Image,StyleSheet,Linking } from 'react-native'
+import Card from '../components/Card'
+import CardSection from '../components/CardSection'
+
+
+const NewsDetail =({news})=>{
+    const {url,urlToImage,title,source} = news;
+    const {imageStyle,titleTextStyle,titleContentStyle} = styles;
+
+return(
+<Card> 
+    <CardSection>
+    <Image
+        style={imageStyle}
+        source={{uri:urlToImage}}/>
+     </CardSection>
+     
+     <CardSection>
+        <View style={titleContentStyle}>
+          <Text style={titleTextStyle}>{title}</Text>
+          <Text>{source.name}</Text>
+        </View>
+      </CardSection>
+
+</Card>
+);
+}
+
+export default NewsDetail;
+
+const styles=StyleSheet.create({
+    imageStyle: {
+        height: 300,
+        flex: 1,
+        width: null,
+        borderTopRightRadius:4,
+        borderTopLeftRadius:4,
+        
+      },
+      titleTextStyle: {
+        fontSize: 18,
+        fontWeight: '600',
+        color:'#000'
+      },
+      titleContentStyle: {
+        justifyContent: "space-around"
+      },
+
+})
